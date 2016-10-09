5e3b79e56cf3de93084a7a63d271dcfc9ab4171f
Allie Kim <yealee.kim87@gmail.com>
2016-10-09 21:16:55 +0900

    remove useless cat function

--- js/util/rename.js
+++ js/util/rename.js
@@ -1,23 +1,24 @@
 import _ from 'lodash';
 
-let cat = (head, ...theArgs) => {
+/*let cat = (head, ...theArgs) => {
     return [head].concat.apply([head], ...theArgs);
-};
-
+};*/
 
 /*
 * usage: rename({a: 1, b: 2, c: 3}, {'a': 'AAA', 'b': 'BBB'})
 * return: {c: 3, AAA: 1, BBB: 3}
+*
+* source from the Functional JavaScript: chapter 2
 * */
 let renameObjKeys = (obj, newNames) => {
-    return _.reduce(newNames, (o, nu, old) => {
-            if (_.has(obj, old)) {
-                o[nu] = obj[old];
-                return o;
+    return _.reduce(newNames, (omitResultObj, newKey/*value*/, oldKey/*key*/) => {
+            if (_.has(obj, oldKey)) {
+                omitResultObj[newKey] = obj[oldKey];
+                return omitResultObj;
             } else
-                return o;
+                return omitResultObj;
         },
-        _.omit.apply(null, cat(obj, _.keys(newNames))));
+        _.omit(obj, _.keys(newNames)));
 };
 
 export {renameObjKeys};
